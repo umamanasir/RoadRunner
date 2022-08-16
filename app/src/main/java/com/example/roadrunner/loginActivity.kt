124f8a50bfd8b547d7c4bb68de32b150ba1ed224
Umama Nasir Abbasi <77013222+umamanasir@users.noreply.github.com>
2022-08-17 03:50:25 +0500

    register page created. Login page connected to register page using setOnClickListener.

--- app/src/main/java/com/example/roadrunner/loginActivity.kt
+++ app/src/main/java/com/example/roadrunner/loginActivity.kt
@@ -1,9 +1,9 @@
 package com.example.roadrunner
-
+import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import android.view.View
 import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 
 class loginActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,13 +12,20 @@ class loginActivity: AppCompatActivity() {
         var email = findViewById(R.id.email) as EditText
         var password = findViewById(R.id.password) as EditText
         var btn_submit = findViewById<Button>(R.id.btnLogin)
+        val registerNow = findViewById<TextView>(R.id.registerNow)
 
         // set on-click listener
         btn_submit.setOnClickListener {
             val email = email.text;
             val password = password.text;
             Toast.makeText(this@loginActivity, email, Toast.LENGTH_LONG).show()
-            // validate + verfiy the user_name and password combination
+            // validate + verify the user_name and password combination
+        }
+
+        registerNow.setOnClickListener{
+            val intent = Intent(this, registerActivity::class.java)
+            startActivity(intent)
+
         }
     }
 }
---
+++ app/src/main/java/com/example/roadrunner/registerActivity.kt
@@ -0,0 +1,28 @@
+package com.example.roadrunner
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.view.View
+import android.widget.*
+
+class registerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        var name = findViewById(R.id.nameRegister) as EditText
+        var email = findViewById(R.id.emailRegister) as EditText
+        var password = findViewById(R.id.passwordRegister) as EditText
+        var btn = findViewById<Button>(R.id.registerBtn)
+        btn.setOnClickListener {
+            var name = name.text
+            val email = email.text;
+            val password = password.text;
+            Toast.makeText(this@registerActivity, email, Toast.LENGTH_LONG).show()
+            // validate + verify the user_name and password combination
+        }
+
+    }
+
+
+}
